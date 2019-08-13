07a44b82ed2ca6ab8eef89f6776f8359dc20aa57
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-13 12:36:15 -0400

    removed canvas

--- src/components/Canvas.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Canvas = () => {
-	const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight]
-}
-
-export default Canvas;
-
